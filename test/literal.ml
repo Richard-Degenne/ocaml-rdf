62461cc6921281987ae3e665894b223760e308c0
Richard Degenne <richdeg2@gmail.com>
2018-12-01 09:44:51 +0100

    Added extrat test to Literal

--- test/literal.ml
+++ test/literal.ml
@@ -34,10 +34,70 @@ let test_no_language () =
   Alcotest.(check (option string)) "language none" None
     (Rdf.Literal.language literal)
 
+let test_of_string_value () =
+  let value = "value" in
+  let language = "en" in
+  let literal = Rdf.Literal.of_string ~language value in
+  Alcotest.(check string) "right value" value (Rdf.Literal.value literal)
+
+let test_of_string_language () =
+  let value = "value" in
+  let language = "en" in
+  let literal = Rdf.Literal.of_string ~language value in
+  Alcotest.(check (option string)) "right value" (Some language)
+    (Rdf.Literal.language literal)
+
+let test_of_string_datatype () =
+  let value = "value" in
+  let language = "en" in
+  let lang_string = "http://www.w3.org/1999/02/22-rdf-syntax-ns#langString" in
+  let literal = Rdf.Literal.of_string ~language value in
+  Alcotest.(check string) "right datatype" lang_string
+    (Rdf.Literal.datatype literal |> Rdf.Iri.to_string)
+
+let test_of_string_no_language () =
+  let value = "value" in
+  let string = "http://www.w3.org/2001/XMLSchema#string" in
+  let literal = Rdf.Literal.of_string value in
+  Alcotest.(check string) "right datatype" string
+    (Rdf.Literal.datatype literal |> Rdf.Iri.to_string)
+
+let test_of_int_value () =
+  let value = 42 in
+  let literal = Rdf.Literal.of_int value in
+  Alcotest.(check string) "right value" "42" (Rdf.Literal.value literal)
+
+let test_of_int_datatype () =
+  let value = 42 in
+  let integer = "http://www.w3.org/2001/XMLSchema#integer" in
+  let literal = Rdf.Literal.of_int value in
+  Alcotest.(check string) "right datatype" integer
+    (Rdf.Literal.datatype literal |> Rdf.Iri.to_string)
+
+let test_of_float_value () =
+  let value = 42.0 in
+  let literal = Rdf.Literal.of_float value in
+  Alcotest.(check string) "right value" "42." (Rdf.Literal.value literal)
+
+let test_of_float_datatype () =
+  let value = 42.0 in
+  let integer = "http://www.w3.org/2001/XMLSchema#double" in
+  let literal = Rdf.Literal.of_float value in
+  Alcotest.(check string) "right datatype" integer
+    (Rdf.Literal.datatype literal |> Rdf.Iri.to_string)
+
 let test_suite = [
   "create invalid", `Quick, test_create_exception;
   "value", `Quick, test_value;
   "datatype", `Quick, test_datatype;
   "language", `Quick, test_language;
   "no language", `Quick, test_no_language;
+  "of_string value", `Quick, test_of_string_value;
+  "of_string language", `Quick, test_of_string_language;
+  "of_string datatype", `Quick, test_of_string_datatype;
+  "of_string no language", `Quick, test_of_string_no_language;
+  "of_int value", `Quick, test_of_int_value;
+  "of_int datatype", `Quick, test_of_int_datatype;
+  "of_float value", `Quick, test_of_float_value;
+  "of_float datatype", `Quick, test_of_float_datatype;
 ]
